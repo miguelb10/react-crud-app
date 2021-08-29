d87dd1bc45b121fee67c183f34963d8c12fd8ef3
Miguel Blas <miguel.blas_03@hotmail.com>
2021-08-29 12:50:10 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ListadoNombres from "./components/ListadoNombres";
+
+function App() {
+  return (
+    <div className="container">
+      <ListadoNombres/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ListadoNombres.jsx
@@ -0,0 +1,11 @@
+import {React, useState } from "react";
+
+const ListadoNombres = () => {
+  return(
+    <div>
+      <h2>Aplicación CRUD</h2>
+    </div>
+  )
+}
+
+export default ListadoNombres;
