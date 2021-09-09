d3aa9fc11b2758259e3216d6ecbf1ebc35b93727
Miguel Blas <miguel.blas_03@hotmail.com>
2021-09-08 20:36:26 -0500

    add edit and validate

--- src/components/ListadoNombres.jsx
+++ src/components/ListadoNombres.jsx
@@ -5,20 +5,43 @@ const ListadoNombres = () => {
 
   const [nombre, setNombre] = useState('');
   const [listaNombres, setListaNombres] = useState([]);
+  const [modoEdicion, setModoEdicion] = useState(false);
+  const [id, setId] = useState('');
+  const [error, setError] = useState(null);
 
   const addNombre = (e)=> {
     e.preventDefault();
+    if(!nombre.trim()){
+      setError('El campo nombre esta vacio');
+      return;
+    }
     const nuevoNombre = {
       id:uniqid(),
       tituloNombre:nombre
     }
     setListaNombres([...listaNombres, nuevoNombre]);
     setNombre('');
+    setError(null);
   }
 
   const deleteNombre = (id) => {
     const nuevaArray = listaNombres.filter(item => item.id !== id);
     setListaNombres(nuevaArray);
+  }
+
+  const editar = (item) => {
+    setModoEdicion(true);
+    setNombre(item.tituloNombre);
+    setId(item.id);
+  }
+
+  const editarNombre = (e) => {
+    e.preventDefault();
+    const nuevoArray = listaNombres.map(
+      item => item.id === id ? {id:id, tituloNombre:nombre} : item);
+    setListaNombres(nuevoArray);
+    setModoEdicion(false);
+    setNombre('');
   }
 
   return(
@@ -35,6 +58,10 @@ const ListadoNombres = () => {
                     onClick={() => {deleteNombre(item.id)}}
                     className="btn btn-danger float-end">Borrar
                   </button>
+                  <button 
+                    onClick={() => {editar(item)}}
+                    className="btn btn-info float-end">Editar
+                  </button>
                 </li>
               )
             }
@@ -42,15 +69,26 @@ const ListadoNombres = () => {
         </div>
         <div className="col">
           <h2>Formulario para añadir nombres</h2>
-          <form onSubmit={(e)=>addNombre(e)} className="form-group">
+          <form onSubmit={modoEdicion ? editarNombre : addNombre} className="form-group">
             <input 
               onChange={(e)=>{setNombre(e.target.value)}} 
               className="form-control mb-3" type="text" 
               placeholder="introduce el nombre"
               value={nombre}
             />
-            <input className="btn btn-info btn-block" type="submit" value="Registrar Nombre"/>
+            <input 
+              className="btn btn-info btn-block" 
+              type="submit" 
+              value={modoEdicion? "Editar Nombre" : "Registrar Nombre"}/>
           </form>
+          {
+            error != null ? (
+              <div className="alert alert-danger">{error}</div>
+
+            ) : (
+              <div></div>
+            )
+          }
         </div>
       </div>
     </div>
